eef2e89a60720789ae81eccf440567d93af7dd38
nereagonzalez12 <ereadoce.ng@gmail.com>
2024-12-05 19:04:50 +0100

    extensible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
 import { CommandsComponent } from './pages/home/commands/commands.component';
 import { ExtensibleComponent } from './pages/home/extensible/extensible.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SidebarComponent } from './pages/home/sidebar/sidebar.component';
 import { StreamComponent } from './pages/home/stream/stream.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
-    SidebarComponent,
     ExtensibleComponent,
     CommandsComponent,
     HomeComponent,
