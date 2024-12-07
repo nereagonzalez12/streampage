cd70af152c8b496e6470ebe872176daf62313271
nereagonzalez12 <ereadoce.ng@gmail.com>
2024-12-07 14:34:00 +0100

    pages finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CommandsComponent } from './pages/home/commands/commands.component';
-import { ExtensibleComponent } from './pages/home/extensible/extensible.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'commands', component: CommandsComponent },
-  { path: 'extensible', component: ExtensibleComponent },
   { path: '**', redirectTo: '', },
 ];
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  homeView: boolean = true;
+  commandView: boolean = false;
+  extensibleView: boolean = false;
+
+
+  showCommandView() {
+    this.commandView = true;
+    this.homeView = false;
+    this.extensibleView = false;
+  }
+
+  showExtensibleView() {
+    this.commandView = false;
+    this.homeView = false;
+    this.extensibleView = true;
+  }
 
 }
